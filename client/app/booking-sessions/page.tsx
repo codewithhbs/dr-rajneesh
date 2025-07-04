385a629542123c7f1b8a54bae6f3697d9abdbaf4
anish-stack <anishjha896@gmail.com>
2025-07-04 12:08:29 +0530

    url-change

--- client/app/booking-sessions/page.tsx
+++ client/app/booking-sessions/page.tsx
@@ -2,12 +2,16 @@ import Bookings from '@/pages/bookings/bookings'
 import { notFound } from 'next/navigation'
 import React from 'react'
 
-export default  async function Page  ({ searchParams })  {
-    const newSearchParams = await searchParams;
-    if(!newSearchParams){
-        return notFound()
-    }
-
-    return <Bookings searchParams={JSON.stringify(newSearchParams)} />
+interface PageProps {
+  searchParams?: {
+    [key: string]: string | string[] | undefined;
+  };
 }
 
+export default function Page({ searchParams }: PageProps) {
+  if (!searchParams) {
+    return notFound();
+  }
+
+  return <Bookings searchParams={JSON.stringify(searchParams)} />;
+}
