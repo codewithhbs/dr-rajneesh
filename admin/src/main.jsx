982931c4e65e0e4100abcbd61f599e5383d76f58
Sac6289 <sachinkumsr59@gmail.com>
2026-06-18 13:32:08 +0530

    fix: update import path for AuthProvider component

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
